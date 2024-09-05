8ff0fc25b1496cfe7623032e6fa44db7dac736e8
Paing Soe Khant <babyboss7916@gmail.com>
2024-09-05 12:35:34 +0630

    Edit Change Role UI

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -8,37 +8,42 @@ use Illuminate\Http\Request;
 class CustomerController extends Controller
 {
     //admin list
-    public function adminList(){
-        $adminList=User::where('role','admin')->orderBy('id','asc')->paginate(10);
-        return view('admin.customer.adminList',compact('adminList'));
+    public function adminList()
+    {
+        $adminList = User::where('role', 'admin')->orderBy('id', 'asc')->paginate(10);
+        return view('admin.customer.adminList', compact('adminList'));
     }
 
     //user list
-    public function userList(){
-        $userList=User::where('role','user')->orderBy('id','asc')->paginate(10);
-        return view('admin.customer.userList',compact('userList'));
+    public function userList()
+    {
+        $userList = User::where('role', 'user')->orderBy('id', 'asc')->paginate(10);
+        return view('admin.customer.userList', compact('userList'));
     }
 
     //delete user
-    public function deleteUser($id){
-        User::where('id',$id)->delete();
-        return back()->with(['deleteSuccess'=>' Deleted Successfully.']);
+    public function deleteUser($id)
+    {
+        User::where('id', $id)->delete();
+        return back()->with(['deleteSuccess' => ' Deleted Successfully.']);
     }
 
     //change role page
-    public function changeRolePage($id){
-        $data=User::where('id',$id)->first();
-        return view('admin.customer.changeRole',compact('data'));
+    public function changeRolePage($id)
+    {
+        $data = User::where('id', $id)->first();
+        return view('admin.customer.changeRole', compact('data'));
     }
 
     //change role
-    public function changeRole(Request $request,$id){
+    public function changeRole(Request $request, $id)
+    {
         $user = User::find($id);
         if ($user) {
             $user->role = $request->role;
             $user->save();
 
-            return redirect()->route('admin#adminList')->with(['changeSuccess'=>'Role updated successfully.']);
+            return redirect()->route('admin#adminList')->with(['changeSuccess' => 'Role Changed successfully.']);
         }
     }
 }
--- resources/views/admin/customer/changeRole.blade.php
+++ resources/views/admin/customer/changeRole.blade.php
@@ -8,28 +8,30 @@
     <div class="container-fluid">
         <h4>{{ now()->toFormattedDayDateString() }}</h4>
         @if (session('changeSuccess'))
-        <div class="col-6 offset-6">
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                <i class="fa-solid fa-check mr-3"></i> {{ session('changeSuccess') }}
-                <a type="" class="btn-close ml-3" data-bs-dismiss="alert" aria-label="Close"><i
-                        class="fa-regular fa-circle-xmark"></i></a>
+            <div class="col-6 offset-6">
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <i class="fa-solid fa-check mr-3"></i> {{ session('changeSuccess') }}
+                    <a type="" class="btn-close ml-3" data-bs-dismiss="alert" aria-label="Close"><i
+                            class="fa-regular fa-circle-xmark"></i></a>
+                </div>
             </div>
-        </div>
-    @endif
+        @endif
         <div class="container mt-5">
             <div class="row justify-content-center">
                 <div class="col-md-6">
                     <div class="card">
                         <div class="card-body">
-                            <form action="{{route('admin#changeRole',$data->id)}}" method="post">
+                            <form action="{{ route('admin#changeRole', $data->id) }}" method="post">
                                 @csrf
                                 <div class="mb-3">
                                     <label for="name" class="form-label">Name</label>
-                                    <input type="text" name="name" class="form-control" value="{{old('name',$data->name)}}" disabled>
+                                    <input type="text" name="name" class="form-control"
+                                        value="{{ old('name', $data->name) }}" disabled>
                                 </div>
                                 <div class="mb-3">
                                     <label for="email" class="form-label">Email</label>
-                                    <input type="email" name="email" class="form-control"  value="{{old('email',$data->email)}}" disabled>
+                                    <input type="email" name="email" class="form-control"
+                                        value="{{ old('email', $data->email) }}" disabled>
                                 </div>
                                 <div class="mb-3">
                                     <label for="role" class="form-label">Role</label>
@@ -40,10 +42,10 @@
                                     </select>
                                 </div>
                                 <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                    <button type="submit" class="btn btn-primary me-md-2 mr-2">Update</button>
-                                   <a href="{{route('admin#adminList')}}">
-                                    <button type="button" class="btn btn-secondary">Back</button>
-                                   </a>
+                                    <button type="submit" class="btn btn-primary me-md-2 mr-2">Change</button>
+                                    <a href="{{ route('admin#adminList') }}">
+                                        <button type="button" class="btn btn-secondary">Back</button>
+                                    </a>
                                 </div>
                             </form>
                         </div>
